2b91e22e68f26a2f0abe7ba4566cc33b974e447f
Arjun <akarjunkumar1136@gmail.com>
2026-09-09 22:45:36 +0500

    added in task3

--- lab-02/task3.s
+++ lab-02/task3.s
@@ -2,12 +2,17 @@
 .globl main 
 main:   
 
-    li x24, 10 
-    li x26, 0x200 
+    li x24, 10 # end value of i 
+    li x26, 0x200  # base address of array a 
 
     
-    blt x22, x24, for 
-
-    for: 
-        sw x, 0(x26) # 
-        
+for1: 
+    blt x22, x24, forBody   # if x22(i) < x24(10), jump to for loop
+    beq x0, x0, EndFor1  
+    forBody: 
+        sw x22, 0(x26) # mem[0+x26] = x22(i)
+        addi x22, x22, 1 
+        addi x26, x26, 4 
+        beq x0, x0 , for1 
+EndFor1: 
+    j EndFor1 
