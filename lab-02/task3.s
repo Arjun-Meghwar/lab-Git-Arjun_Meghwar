62be7d9e6a7115a72df6e135158dde54e808d5f4
Arjun <akarjunkumar1136@gmail.com>
2026-09-15 10:12:36 +0500

    task3_lab2

--- lab-02/task3.s
+++ lab-02/task3.s
@@ -1,18 +1,31 @@
-.text 
-.globl main 
-main:   
+.text
+.globl main
 
-    li x24, 10 # end value of i 
-    li x26, 0x200  # base address of array a 
+main:
+    li x24, 10          # loop bound
+    li x26, 0x200        # base address of array a
+    li x23, 0             # sum = 0 (missing before!)
 
-    
-for1: 
-    blt x22, x24, forBody   # if x22(i) < x24(10), jump to for loop
-    beq x0, x0, EndFor1  
-    forBody: 
-        sw x22, 0(x26) # mem[0+x26] = x22(i)
-        addi x22, x22, 1 
-        addi x26, x26, 4 
-        beq x0, x0 , for1 
-EndFor1: 
-    j EndFor1 
+    li x22, 0             # i = 0
+for1:
+    bge x22, x24, exitFor1
+    slli x7, x22, 2        # x7 = i * 4
+    add  x5, x26, x7        # x5 = &a[i]  
+    sw   x22, 0(x5)         # a[i] = i
+    addi x22, x22, 1
+    beq  x0, x0, for1
+exitFor1:
+
+    li x22, 0             # reset i = 0 
+for2:
+    bge x22, x24, exitFor2
+    slli x5, x22, 2        # x5 = i * 4
+    add  x6, x26, x5        # x6 = &a[i]
+    lw   x7, 0(x6)          # x7 = a[i]
+    add  x23, x23, x7        # sum += a[i]
+    addi x22, x22, 1
+    beq  x0, x0, for2
+exitFor2:
+
+end:
+    j end
