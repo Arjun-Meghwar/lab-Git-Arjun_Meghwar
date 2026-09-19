2f172483954375896cc7f772a02162580a3c32be
Arjun <akarjunkumar1136@gmail.com>
2026-09-19 14:17:49 +0500

    lab_3 task3 swaps complete

--- lab-03/task3.s
+++ lab-03/task3.s
@@ -1,43 +1,54 @@
 .text 
 .globl main
 main:
-    addi sp, sp, -8
 
-    sw x28, 0(sp) # save x28 on stack
+    # Setting Initial Data
+    li x28, 5 
+    li x29, 10 
+   
+    addi sp, sp, -8 
+    sw x28, 4(sp)
+    sw x29, 0(sp) 
 
-    jal x1, swap 
+    add x10, sp, x0 
+    li x11, 0 # swapping 0-index & 1-index 
+    
+    jal x1, swap  # calling swap 
+    j end # exit the main procedure  
+    swap: 
+        
+       
 
-    lw x28, 0(sp) # restore x28 from stack 
+        slli x5, x11, 2 
+        add x5, x10, x5 
+        lw x5, 0(x5) # kth eleement 
+
+        
+        addi x6, x11, 1 # for k(x11) + 1 
+        slli x6, x6, 2  # 4 * (k + 1) 
+        add x6, x10, x6 # address of (k+1)th element = baseAddress + 4 * (k + 1) 
+        lw x7, 0(x6) # loading (k+1)th index original value for later assignment
+        sw x5, 0(x6) # storing kth on k+1 th ele 
+
+        # reutilizing the x5 temporay register 
+        slli x5, x11, 2 # k * 4 
+        add x5, x10, x5 # address  of kth element = baseAddress + k * 4 
+        sw x7, 0(x5) # storing x7(original value of (k+1)th index) at address  of kth element
+
+
+        jalr x0, 0(x1) 
+    addi sp, sp , 8 # releasing array from stack 
+
+end:
+ j end
+
+
+
+
 
     
 
-    swap: 
         
-
-        # slli x5, x11, 2  # k * 4 
-
-        # add x6, x10, x5  # base address + k * 4 
-
-        
-
-        # lw x7, 0(x6)  # v[k] 
-
-        # addi x28, x7, 0  # store v[k] in x28
-
-        # addi x29, x11, 1 # k + 1 
-
-        # slli x8, x29, 2  # (k + 1) * 4
-        # add x25, x10, x8  # base address + (k + 1) * 4
-
-        # lw x9, 0(x25)  # x9 = v[k + 1] 
-
-        
-        # sw x9, 0(x6) # store v[k + 1] in v[k]
-
-        # sw x28, 0(x25) # store v[k] in  v[k+1] 
-
-        # jalr x0, 0(x1)
-
 
 
 
