f00b39d193c547c3639612567487ae0d950385fb
Arjun <akarjunkumar1136@gmail.com>
2026-09-20 12:45:20 +0500

    added_inlab3_task4

--- lab-03/task4.s
+++ lab-03/task4.s
@@ -2,29 +2,40 @@
 .globl main
 
 main: 
+    li x11, 0x200 # source 
+    li x10, 0x400 # destination 
 
-    j x1, strcpy
+    # Storing sample values in source(x11) 
+    li x28, 1 
+    sb x28, 0(x11)
+    li x28, 3 
+    sb x28, 1(x11)
+    li x28, 5 
+    sb x28, 2(x11)
+
+    jal x1, strcpy
     j end 
 
     strcpy: 
         addi sp, sp, -8 
-        sd x19, 0(sp) 
+        sw x19, 0(sp) 
         li x19, 0 # i 
 
         while: 
         # get y[i] 
         add x5, x11, x19 # address of y[i] 
         add x6, x10, x19 # address of x[i] 
-        lb x7, 0(x5) # loading value at address of y[i]-> (x5) 
+        lb x7, 0(x5) # loading value at address of y[i]-> (x5)
+        beq x7, x0, exitStrCpy  # if y[i] == '\0'  
         sb x7, 0(x6) # store y[i] at address of x[i] 
     
-        beq x7, x0, exitStrCpy  # if y[i] == '\0' 
+       
 
         addi x19, x19, 1 # i++ 
         beq x0, x0, while
 
         exitStrCpy: 
-            ld x19, 0(sp) 
+            lw x19, 0(sp) 
             addi sp, sp, 8 
             jalr x0, 0(x1)  
 
